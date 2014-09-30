92289548d506416292284c634f6b3de88c9c5558
TeamA-ict <sachila_sunchi@yahoo.com>
2014-09-30 14:03:17 +1000

    f

---
+++ controller.php
@@ -0,0 +1,7 @@
+<?php
+
+$con = pg_connect("host=ec2-54-197-241-79.compute-1.amazonaws.com dbname=d2br0epq9lipet   
+ user= izyrzypyhztonp  
+ password=W0iB4Hq87N5WFaLrWPN1aQWcdt") or die("Could not connect:".pg_last_error());
+
+?>
