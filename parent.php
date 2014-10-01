5cdec46b53087893f0b95106010f0337c05dbe25
TeamA-ict <sachila_sunchi@yahoo.com>
2014-10-01 14:12:48 +1000

    g

--- parent.php
+++ parent.php
@@ -2,15 +2,21 @@
 	$conn_string = "host=ec2-54-197-241-79.compute-1.amazonaws.com port=5432 dbname=d2br0epq9lipet user=izyrzypyhztonp password=W0iB4Hq87N5WFaLrWPN1aQWcdt";
 $dbconn4 = pg_connect($conn_string);
 	
-	 $result = pg_query("select id from parents;");
+	 $conn = pg_pconnect("dbname=d2br0epq9lipet");
+if (!$conn) {
+  echo "An error occurred.\n";
+  exit;
+}
+
+$result = pg_query($conn, "SELECT id FROM parents");
 if (!$result) {
   echo "An error occurred.\n";
+  
 }
 else {
-	echo "sachila";
+	echo "  gagan \n ";
 }
-	 
 	   
 	   echo "Hello";
 	
-	?>
+?>
