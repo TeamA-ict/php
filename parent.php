2f7afd8f3870025237a5b20d394a3620e30a7c67
TeamA-ict <sachila_sunchi@yahoo.com>
2014-10-01 17:22:25 +1000

    php

--- parent.php
+++ parent.php
@@ -1,7 +1,7 @@
  <?php
 	$conn_string = "host=ec2-54-197-241-79.compute-1.amazonaws.com port=5432 dbname=d2br0epq9lipet user=izyrzypyhztonp password=W0iB4Hq87N5WFaLrWPN1aQWcdt";
 $dbconn4 = pg_connect($conn_string);
-	$result = pg_query($conn, "SELECT * FROM parents");
+	$result = pg_query("SELECT * FROM parents");
 if (!$result) {
   echo "An error occurred.\n";  
 }
