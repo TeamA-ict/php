bcf364a0811fc42fbc2e36a7ba6f1ed7774ae45a
TeamA-ict <sachila_sunchi@yahoo.com>
2014-10-01 17:19:42 +1000

    php

--- parent.php
+++ parent.php
@@ -1,14 +1,13 @@
  <?php
 	$conn_string = "host=ec2-54-197-241-79.compute-1.amazonaws.com port=5432 dbname=d2br0epq9lipet user=izyrzypyhztonp password=W0iB4Hq87N5WFaLrWPN1aQWcdt";
 $dbconn4 = pg_connect($conn_string);
-	$query = " select * from project ";
-	$result = pg_query($query) or die("Query failed:".pg_last_error());
-	echo "<table border='1' align='center'>
-	<tr>
-	<td>  id </td>
-	<td> name </td>
-	</tr>
-	</table>\n";
+	$result = pg_query($conn, "SELECT * FROM parents");
+if (!$result) {
+  echo "An error occurred.\n";  
+}
+else {
+	echo "gagan \n ";
+}
 	 
 	   
 	   echo "Hello";
