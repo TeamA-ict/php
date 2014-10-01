13c3d1839f89cabc828e40ab3dfe444e12ea5feb
TeamA-ict <sachila_sunchi@yahoo.com>
2014-10-01 13:57:03 +1000

    g

--- parent.php
+++ parent.php
@@ -1,11 +1,13 @@
  <?php
-	require 'controller.php';
+	$conn_string = "host=ec2-54-197-241-79.compute-1.amazonaws.com port=5432 dbname=d2br0epq9lipet user=izyrzypyhztonp password=W0iB4Hq87N5WFaLrWPN1aQWcdt";
+$dbconn4 = pg_connect($conn_string);
 	
-	 $query  = "select * from parents";
+	 $result = pg_query($conn_string, "SELECT id,links FROM parents");
+if (!$result) {
+  echo "An error occurred.\n";
+  exit;
+}
 	 
-	  $result = pg_query($query) or die("Query failed:".pg_last_error());
-	  
-	   pg_free_result($result);
 	   
 	   echo "Hello";
 	
